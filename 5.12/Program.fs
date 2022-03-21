e15d02333a6e2a79a640b3c207e56872da99b9f2
Кристина Вячеславовна Стасюк <s0160150@edu.kubsu.ru>
2022-03-22 00:10:21 +0300

    Добавлена 12 задача 5 лабы

---
+++ 5.12/Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let Otvet (k:string) =    
+   match k with 
+   |"F#"|"Prolog" -> "Поздравляю, Вы - подлиза!"
+   | _ -> "Как можно не любить F#!? "
+[<EntryPoint>]
+let main argv =
+   System.Console.WriteLine("Какой ваш любимый язык программирования?")
+   //Случай суперпозиции
+   (Console.ReadLine>>Otvet>>Console.WriteLine)()
+   System.Console.WriteLine("Какой ваш любимый язык программирования?")
+  // Случай каррирования
+   let F y x z =z(x(y()))
+   F Console.ReadLine Otvet Console.WriteLine
+   0 
