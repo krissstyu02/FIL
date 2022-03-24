44ebc4f2d27e7f9e1a1f780bf8cb98439ba7d863
Кристина Вячеславовна Стасюк <s0160150@edu.kubsu.ru>
2022-03-24 22:55:01 +0300

    5.18 пункт в

--- 5.18/Program.fs
+++ 5.18/Program.fs
@@ -1,14 +1,15 @@
 ﻿open System
 // а
+let nod x y =
+    let rec nod1 x y z newnod= 
+        if x+1 = z || y+1 = z then newnod
+        else 
+            if x%z <> 0 || y%z <> 0 then nod1 x y (z+1) newnod
+            else 
+                let nod = z
+                nod1 x y (z+1) nod
+    nod1 x y 1 1
 
-//нод
-let rec nod a b =
-    if a=b then a
-    else 
-        if a>b then nod (a-b) b 
-        else nod a (b-a) 
-
-//операции с  взаимно-простыми числами
 let obh x func init =
     let rec obh2 x func init del =
         if del=0 then init
@@ -18,21 +19,16 @@ let obh x func init =
             obh2 x func init2 del2
     obh2 x func init x
 
-//количество
 let inc x = 
     obh x (fun x y-> x + 1) 0
 
 //б
 
-//кратный 3
-let check1 x=x%3=0
-
-//сумма цифр,удовлетворяющая условию
-let sum3 x с = 
+let sum3 x = 
     let rec sum3 x sum = 
         if x = 0 then sum
         else
-            if  с (x%10) then
+            if x%10%3 = 0 then
                 let sum1 = sum + x%10
                 let x1 = x/10
                 sum3 x1 sum1
@@ -41,8 +37,8 @@ let sum3 x с =
                 sum3 x1 sum
     sum3 x 0
 
-
 //в
+
 
 //количество цифр данного числа взаимно-простых с делителем
 let countnum x del = 
@@ -76,6 +72,6 @@ let main argv =
     System.Console.WriteLine("Введите число")
     let x = System.Convert.ToInt32(System.Console.ReadLine())
     System.Console.WriteLine("Колличество взаимно-простых с x:{0}", inc x )
-    System.Console.WriteLine("Сумма цифр числа, делящихся на 3 :{0}", sum3 x check1 )
-    System.Console.WriteLine("Делитель числа, являющийся взаимно простым с наибольшим количеством цифр данного числа. :{0}", finddel x )
+    System.Console.WriteLine("Сумма цифр числа, делящихся на 3 :{0}", sum3 x )
+    System.Console.WriteLine("Делитель числа, являющийся взаимно простым снаибольшим количеством цифр данного числа. :{0}", finddel x )
     0 
