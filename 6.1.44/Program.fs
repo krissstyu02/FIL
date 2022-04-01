93addeac9146203a21f06d700e0844d92fc6cf0c
Кристина Вячеславовна Стасюк <s0160150@edu.kubsu.ru>
2022-04-01 18:17:22 +0300

    6.44 :(

---
+++ 6.1.44/Program.fs
@@ -0,0 +1,32 @@
+﻿// Learn more about F# at http://fsharp.org
+
+//Дан массив чисел. Необходимо проверить, чередуются ли в нем целые и вещественные числа.
+open System
+
+let rec readlistdouble n =
+    if n = 0 then []
+    else
+    let Head = Console.ReadLine()|>Convert.ToDouble
+    let Tail = readlistdouble (n-1)
+    Head::Tail
+
+let readData = 
+    Console.WriteLine("Введите размерность списка:  ")
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    Console.WriteLine("Введите список: ")
+    readlistdouble n
+
+let check (list:float list)=
+    let rec check2 list b=
+        match list with
+        | []-> true
+        | h::t-> 
+            if ((h%1.0=0.0 && b%1.0=0.0) ||(h%1.0<>0.0 && b%1.0<>0.0)) then false     //проверяем обратное
+            else check2 t h
+    check2 list.Tail list.Head
+
+[<EntryPoint>]
+let main argv =
+   let list = readData 
+   Console.WriteLine(check list)
+   0 // return an integer exit code
