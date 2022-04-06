a823245710f5115f78c9c477160bf29e4df0630d
Кристина Вячеславовна Стасюк <s0160150@edu.kubsu.ru>
2022-04-06 21:11:45 +0300

    7.12

---
+++ 7.12/Program.fs
@@ -0,0 +1,36 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+let rec readList n =
+    List.init(n) (fun index->Console.ReadLine()|>Int32.Parse)
+
+let readData = 
+    Console.WriteLine("Введите размерность списка:  ")
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    Console.WriteLine("Введите список: ")
+    readList n
+
+let rec writeList = function
+    [] ->   let z = System.Console.ReadKey()
+            0
+    | (head : int)::tail -> 
+                       System.Console.WriteLine(head)
+                       writeList tail  
+
+//переставить в обратном порядке элементы массива, расположенные между его минимальным и максимальным элементами
+
+let min list=List.findIndex(fun x->x=List.min list) list
+let max list=List.findIndex(fun x->x=List.max list) list
+
+let resh list= 
+    let start=Math.Min(max list,min list)
+    let finish=Math.Max(min list,max list)
+    let pieces=list.[start+1..finish-1]
+    list.[0..start]@ (List.rev pieces) @ list.[finish..list.Length-1]
+
+[<EntryPoint>]
+let main argv =
+    let l=readData
+    Console.WriteLine("Новый список:")
+    writeList(resh l)
+    0 
