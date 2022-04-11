a1d91094cfe3142a2fa80137f4a2eb4ee4e60cf7
Кристина Вячеславовна Стасюк <s0160150@edu.kubsu.ru>
2022-04-11 13:52:06 +0300

    7 лаба 18 номер вариант 2

---
+++ 7.18.2/Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+(*Дан массив А [1; 2; 3;] и массив B [4; 5; 7] скопировать
+последний элемент массива В в массив А.*)
+[<EntryPoint>]
+let main argv =
+    let arrayA=[|1;2;3|]
+    let arrayB=[|4;5;7|]
+    let new_array=Array.append arrayA [|(Array.item(arrayB.Length-1) arrayB)|] 
+    Console.WriteLine("arrayA=[|1;2;3|] arrayB=[|4;5;7|]
+Новый массив=")
+    printfn"%A"new_array
+
+    0 // return an integer exit code
+
+
+
