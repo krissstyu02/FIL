c53a44ec5a953234aa63498f835dbdfe257fdb95
Кристина Вячеславовна Стасюк <s0160150@edu.kubsu.ru>
2022-04-16 12:51:16 +0300

    7 лаба 19 номер варинат 2 пункт 1

---
+++ 7.19.2/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+//задача 2
+
+let ordered str=
+    let new_string=String.filter(fun ch->Char.IsLower(ch)) str      //переводим символы сьроки на нижний регистр
+    let make_list=Seq.toList(new_string)
+    let check=List.map2(fun ch1 ch2-> ch1<=ch2) make_list.[0..make_list.Length-2] make_list.[1..make_list.Length-1]   //проверка на упорядоченность двух элементов
+    if (List.fold (fun s x-> if x=false then s+1 else s) 0 check) = 0 then Console.WriteLine("Символы строки упорядочены")   
+    else Console.WriteLine("Символы строки  не упорядочены")
