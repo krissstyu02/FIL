838e129d78dac0d9313209bdab9d8c8a004ba815
Кристина Вячеславовна Стасюк <s0160150@edu.kubsu.ru>
2022-04-16 13:03:37 +0300

    задача 17

--- 7.19.2/Program.fs
+++ 7.19.2/Program.fs
@@ -17,4 +17,21 @@ let letterA str=
     let new_string=String.filter(fun ch->ch='A') str
     Console.WriteLine(new_string.Length)
 
+//задача 17
+
+let find_file str=
+    let rec file2 (str: string) check ind =
+        if (ind = -1) then str     //дошли до начала строки
+        else
+            if (int(str.[ind]) = check) then str.[(ind+1)..]  //добавляем название файла
+            else file2 str check (ind-1)
+    let rec file3 (str: string) check ind =
+        if (ind = str.Length) then str      //дошли до конца строки
+        else
+            if (int(str.[ind]) = check) then str.[..(ind-1)]  //добавляем название файла 
+            else file3 str check (ind+1)
+    let file_name1= file2 str (int('\\')) (str.Length-1)
+    let file_name2=file3 file_name1 (int('.')) 0
+    Console.WriteLine(file_name2)
+
 
