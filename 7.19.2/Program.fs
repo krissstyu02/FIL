151622c2d723f91247ea5727315e93d4227b880b
Кристина Вячеславовна Стасюк <s0160150@edu.kubsu.ru>
2022-04-16 12:56:18 +0300

    пункт 10

--- 7.19.2/Program.fs
+++ 7.19.2/Program.fs
@@ -10,3 +10,11 @@ let ordered str=
     let check=List.map2(fun ch1 ch2-> ch1<=ch2) make_list.[0..make_list.Length-2] make_list.[1..make_list.Length-1]   //проверка на упорядоченность двух элементов
     if (List.fold (fun s x-> if x=false then s+1 else s) 0 check) = 0 then Console.WriteLine("Символы строки упорядочены")   
     else Console.WriteLine("Символы строки  не упорядочены")
+
+//задача 10
+
+let letterA str=
+    let new_string=String.filter(fun ch->ch='A') str
+    Console.WriteLine(new_string.Length)
+
+
