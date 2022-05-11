5ecbdeefb39899e8a7ab44b3781cac430e5b2673
Кристина Вячеславовна Стасюк <s0160150@edu.kubsu.ru>
2022-05-11 14:12:11 +0300

    8 лаба ЛР 7

---
+++ 8.4/Program.fs
@@ -0,0 +1,29 @@
+﻿(*С использованием класса MailboxProcessor реализуйте агента, 
+который реагирует на внешние события и выполняет различные действия 
+(например, выдает результаты в консоль)*)
+
+
+open System
+
+// Агент выводит лишь маленькие буквы сообщения
+let printerAgent = MailboxProcessor.Start(fun inbox->
+    // обработка сообщений
+    let rec messageLoop() = async{
+        // чтение сообщения
+        let! msg = inbox.Receive()
+        // печать сообщения
+        Console.WriteLine("Строка после преобразования: " + String.filter (fun x -> Char.IsUpper x) msg)
+        return! messageLoop()
+        }
+        // запуск обработки сообщений
+    messageLoop()
+
+    )
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Введите строку для преобразования")
+    let k=System.Convert.ToString(System.Console.ReadLine())
+    let arg1 = printerAgent.Post(k)
+    Console.ReadKey()
+    0 // return an integer exit code
